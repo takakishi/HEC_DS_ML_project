d5893b4f0362de1c1741b87f22221de3903e799d
takakishi <takaaki.kishida.9@gmail.com>
2023-12-10 21:25:58 +0100

    Create streamlit_app.py

---
+++ src/streamlit_app.py
@@ -0,0 +1,9 @@
+# streamlit_app.py
+import streamlit as st
+
+st.title('My Streamlit App')
+# Add more Streamlit widgets to interact with the model
+
+# bash ------------------------------------------
+# pip install streamlit
+# streamlit run streamlit_app.py
